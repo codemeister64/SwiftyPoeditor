a24040564fe1ed88d0fd87501f7f7eec81b8e7c2
Oleksandr Vitruk <vitruk.o.v@gmail.com>
2020-09-16 15:52:31 +0300

    Reason: Fix HTTP requests with new NIO version Summary: Replaced IBM SwiftyRequest with AsyncHTTPClient, updated packages to latest versions

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/yanagiba/swift-ast.git", .exact("0.19.9")),
-        .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .exact("3.1.0")),
-        .package(url: "https://github.com/vapor/console-kit.git", .exact("4.2.1"))
+        .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "swifty-poeditor",
-            dependencies: ["SwiftAST+Tooling", "SwiftyRequest", "ConsoleKit"]),
+            dependencies: ["SwiftAST+Tooling", "AsyncHTTPClient", "ConsoleKit"])
     ],
     swiftLanguageVersions: [.v5]
 )
